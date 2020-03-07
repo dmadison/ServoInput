39f82caa43691444a4fbe126f5cafed69d486e47
David Madison <dmadison@users.noreply.github.com>
2020-03-07 03:43:09 -0500

    Create Remapping example

---
+++ examples/Remapping/Remapping.ino
@@ -0,0 +1,84 @@
+/*
+ *  Project     Servo Input Library
+ *  @author     David Madison
+ *  @link       github.com/dmadison/ServoInput
+ *  @license    LGPLv3 - Copyright (c) 2020 David Madison
+ *
+ *  This example is part of the Arduino Servo Input Library.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Example:      Remapping
+ *  Description:  Reads a servo signal and then remaps it to angle, percent,
+ *                and two arbitrary integer values.
+ */
+
+#include <ServoInput.h>
+
+/* The signal pin for ServoInput MUST be an interrupt-capable pin!
+ *     Uno, Nano, Mini (328P): 2, 3
+ *     Micro, Leonardo (32U4): 0, 1, 2, 3, 7
+ *             Mega, Mega2560: 2, 3, 18, 19, 20, 21
+ * Reference: https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
+ */
+ServoInputPin<2> servo;
+
+void setup() {
+	Serial.begin(115200);
+
+	ServoInput.begin();
+
+	while (servo.available() == false) {
+		Serial.println("Waiting for servo signal...");
+		delay(500);
+	}
+}
+
+void loop() {
+	// Use the built-in angle map (float, 0 - 180)
+	float angle = servo.getAngle();
+
+	// Use the built-in percentage map (float, 0 - 100)
+	float percent = servo.getPercent();
+
+	// Map to a single byte, unsigned
+	byte singleByte = servo.map(0, 255);
+
+	// Map to a signed integer
+	int signedInt = servo.map(-32768, 32767);
+
+	Serial.print("Servo Map | ");
+
+	Serial.print("A: ");
+	Serial.print(angle);
+	Serial.print("deg");
+
+	Serial.print("  ");
+
+	Serial.print("P: ");
+	Serial.print(percent);
+	Serial.print("%");
+
+	Serial.print(" | ");
+
+	Serial.print("Byte: ");
+	Serial.print(singleByte);
+
+	Serial.print("  ");
+
+	Serial.print("Int: ");
+	Serial.print(signedInt);
+
+	Serial.println();
+}
