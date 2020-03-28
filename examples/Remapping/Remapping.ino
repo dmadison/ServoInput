6844eef0f07e243fccef7c2c0ec50041ae1ee71a
David Madison <dmadison@users.noreply.github.com>
2020-03-28 02:16:05 -0400

    Group remap example functions calls with prints
    Easier to follow

--- examples/Remapping/Remapping.ino
+++ examples/Remapping/Remapping.ino
@@ -46,19 +46,10 @@ void setup() {
 }
 
 void loop() {
+	Serial.print("Servo Map | ");
+
 	// Use the built-in angle map (float, 0 - 180)
 	float angle = servo.getAngle();
-
-	// Use the built-in percentage map (float, 0 - 100)
-	float percent = servo.getPercent();
-
-	// Map to a single byte, unsigned
-	byte singleByte = servo.map(0, 255);
-
-	// Map to a signed integer
-	int signedInt = servo.map(-32768, 32767);
-
-	Serial.print("Servo Map | ");
 
 	Serial.print("A: ");
 	Serial.print(angle);
@@ -66,16 +57,25 @@ void loop() {
 
 	Serial.print("  ");
 
+	// Use the built-in percentage map (float, 0 - 100)
+	float percent = servo.getPercent();
+
 	Serial.print("P: ");
 	Serial.print(percent);
 	Serial.print("%");
 
 	Serial.print(" | ");
 
+	// Map to a single byte, unsigned
+	byte singleByte = servo.map(0, 255);
+
 	Serial.print("Byte: ");
 	Serial.print(singleByte);
 
 	Serial.print("  ");
+
+	// Map to a 16-bit signed integer
+	int signedInt = servo.map(-32768, 32767);
 
 	Serial.print("Int: ");
 	Serial.print(signedInt);
